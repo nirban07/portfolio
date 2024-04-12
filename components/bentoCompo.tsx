ea623642dd4b0430daea784c15b4e6831555a1da
nirban07 <nirban012@gmail.com>
2024-04-13 00:38:29 +0530

    first commit

---
+++ components/bentoCompo.tsx
@@ -0,0 +1,80 @@
+"use client"
+import { link } from 'fs';
+import Image from 'next/image';
+import React from 'react';
+
+import { cn } from '@/utils/cn';
+import {
+	IconArrowWaveRightUp, IconBoxAlignRightFilled, IconBoxAlignTopLeft, IconClipboardCopy,
+	IconFileBroken, IconSignature, IconTableColumn
+} from '@tabler/icons-react';
+
+import selfImage from '../public/application.png';
+import { BentoGrid, BentoGridItem } from './ui/bento-grid';
+
+export function BentoGridDemo() {
+	return (
+		<BentoGrid className="max-w-4xl mx-auto">
+			{items.map((item, i) => (
+				<BentoGridItem
+					key={i}
+					title={item.title}
+					description={item.description}
+					header={item.header}
+					icon={item.icon}
+					link={item.link}
+					className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+				/>
+			))}
+		</BentoGrid>
+	);
+}
+const Skeleton = () => (
+	<div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
+const items = [
+	{
+		title: "Blogging application",
+		description: "Exploring fast blogging experience with cloudflare.",
+		header: <Image src={selfImage} alt='test' className=' object-cover ' />,
+		icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+		link: "https://medium-frontend-gamma.vercel.app/"
+	},
+	{
+		title: "The Digital Revolution",
+		description: "Dive into the transformative power of technology.",
+		header: <Skeleton />,
+		icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Art of Design",
+		description: "Discover the beauty of thoughtful and functional design.",
+		header: <Skeleton />,
+		icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Power of Communication",
+		description:
+			"Understand the impact of effective communication in our lives.",
+		header: <Skeleton />,
+		icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+	},
+	{
+		title: "The Pursuit of Knowledge",
+		description: "Join the quest for understanding and enlightenment.",
+		header: <Skeleton />,
+		icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+	},
+	// {
+	// 	title: "The Joy of Creation",
+	// 	description: "Experience the thrill of bringing ideas to life.",
+	// 	header: <Skeleton />,
+	// 	icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+	// },
+	// {
+	// 	title: "The Spirit of Adventure",
+	// 	description: "Embark on exciting journeys and thrilling discoveries.",
+	// 	header: <Skeleton />,
+	// 	icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+	// },
+];
---
+++ components/lampDemo.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import React from 'react';
+
+import { LampContainer } from './ui/lamp';
+
+export function LampDemo() {
+	return (
+		<LampContainer>
+			<motion.h1
+				initial={{ opacity: 0.5, y: 100 }}
+				whileInView={{ opacity: 1, y: 0 }}
+				transition={{
+					delay: 0.3,
+					duration: 0.8,
+					ease: "easeInOut",
+				}}
+				className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+			>
+				Lets stay <br /> connected.
+				<div className='flex gap-6 md:gap-8 justify-center mt-4 md:mt-12'>
+					<Link href="https://github.com/nirban07" target='_blank'>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-github text-[#1FC6E2]"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>
+					</Link>
+
+					<Link href="https://linkedin.com/in/nirbanroy" target='_blank'>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-linkedin text-[#1FC6E2]"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" /><rect width="4" height="12" x="2" y="9" /><circle cx="4" cy="4" r="2" /></svg>
+					</Link>
+					<Link href="https://twitter.com/NirbanRoy19" target='_blank'>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-twitter text-[#1FC6E2]"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" /></svg>
+					</Link>
+					<Link href="mailto:nirban012@gmail.com" target='_blank'>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mail text-[#1FC6E2]"><rect width="20" height="16" x="2" y="4" rx="2" /><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" /></svg>
+					</Link>
+				</div>
+			</motion.h1>
+		</LampContainer>
+	);
+}
